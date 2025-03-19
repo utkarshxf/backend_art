c794db8ac3a114eae46a5b5076432af410b9676e
utkarsh <utkarshsinghrajput16@gmail.com>
2025-03-19 11:41:15 +0530

    feat : updated code

--- src/main/java/com/basic/JWTSecurity/artwork_server/model/get_models/GetArtwork.java
+++ src/main/java/com/basic/JWTSecurity/artwork_server/model/get_models/GetArtwork.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class GetArtwork {
+    private String id;
     private String title;
     private Status status;
     private StorageType storageType;
