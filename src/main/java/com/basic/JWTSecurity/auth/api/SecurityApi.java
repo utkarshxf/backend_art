a896c5b36bd44657f49663fadd1544c428f76fd9
utkarsh <utkarshsinghrajput16@gmail.com>
2025-11-04 07:54:40 +0530

    feat : updated code

--- src/main/java/com/basic/JWTSecurity/auth/api/SecurityApi.java
+++ src/main/java/com/basic/JWTSecurity/auth/api/SecurityApi.java
@@ -24,7 +24,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@RestController("/security")
+@RestController
+@RequestMapping("/security")
 public class SecurityApi {
     @Autowired
     private JwtUtils jwtUtils;
